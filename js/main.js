18d65789826368d2d452cf59948c18d33544f9a1
Daniel Campbell <Mavarius@Davids-MacBook-Pro-3.local>
2016-09-13 23:42:03 -0700

    Bug Fixes and addition of a button to choose prefect.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ $(document).ready(init)
 function init() {
   $('#add').click(addStudents);
   $('#form_teams').click(formTeams);
+  $('#prefect').click(choosePrefect);
 
+  // declare global variables
   var $list;
   var totalStudents = [];
 
+  // handler for Add Students button
   function addStudents() {
     var $nameInput = $('#name');
 
@@ -15,7 +18,6 @@ function init() {
     $nameInput.val('');
 
     // strip trailing spaces
-    // FIXME: Double space entries
     names = names.map(function(item, index){
       if (item[0] == " ") {
         item = item.slice(1,item.length);
@@ -37,67 +39,72 @@ function init() {
     }
   }
 
+  // handler for Form Teams button
   function formTeams() {
-    $list = $('#list').children();
+    $("#teams").children().remove();
     var $teamInput = $('#team_size');
     var teamSize = $teamInput.val();
 
-    var teamSet = {};
-    for (var i = 0; i < teamSize; i++) {
-      teamSet[i] = [];
-    }
-
-    for (var i = 0; i < teamSize; i++) {
-      $("#teams").append("<ul class='team'><h4>TEAM "+(i+1)+"</h4></ul>");
-    }
-
-    var assigner = 0;
-    var teamCache = [...totalStudents];
-    shuffle(teamCache);
-
-    while (teamCache.length > 0) {
-      var lastItem = teamCache.length-1;
-      teamSet[assigner].push(teamCache[lastItem]);
-      teamCache = teamCache.splice(0, lastItem);
-
-      if (assigner < teamSize-1) {
-        assigner++;
-      } else {
-        assigner = 0;
+    if (teamSize > 0) {
+      var teamSet = {};
+      for (var i = 0; i < teamSize; i++) {
+        teamSet[i] = [];
       }
-    }
 
-    for (var x = 0; x < teamSize; x++) {
-      for (var y = 0; y < teamSet[x].length; y++) {
-        $("#teams .team:nth-of-type("+(x+1)+")").append("<li>"+teamSet[x][y]+"</li>");
+      for (var i = 0; i < teamSize; i++) {
+        $("#teams").append("<ul class='team'><h4>TEAM "+(i+1)+"</h4></ul>");
       }
+
+      var assigner = 0;
+      var teamCache = [...totalStudents];
+      shuffle(teamCache);
+
+      while (teamCache.length > 0) {
+        var lastItem = teamCache.length-1;
+        teamSet[assigner].push(teamCache[lastItem]);
+        teamCache = teamCache.splice(0, lastItem);
+
+        if (assigner < teamSize-1) {
+          assigner++;
+        } else {
+          assigner = 0;
+        }
+      }
+
+      for (var x = 0; x < teamSize; x++) {
+        for (var y = 0; y < teamSet[x].length; y++) {
+          $("#teams .team:nth-of-type("+(x+1)+")").append("<li>"+teamSet[x][y]+"</li>");
+        }
+      }
+    } else {
+      $("#teams").append("<p>Input must be greater than 0.</p>")
+    }
+  }
+
+  // handler for Choose Prefect Button
+  function choosePrefect() {
+    $list = $('#list').children();
+    var rand = Math.floor(Math.random() * $list.length);
+    var prefect = $list[rand].innerHTML;
+    $(".prefect_pick").text(`Prefect: ${prefect}`);
+  }
+
+  // array shuffler
+  function shuffle(array) {
+    var counter = array.length;
+
+    while (counter > 0) {
+      var index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      var temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
     }
 
-    // chooseCaptain();
+    return array;
   }
 }
 
-function chooseCaptain() {
-  var rand = Math.floor(Math.random() * $list.length);
-  var captain = $list[rand].innerHTML;
-  $("#teams").append("<p class='.captain'> Team Captain: "+captain+"</p>");
-  $("#teams").append(`<p class='.captain'> Team Captain: ${captain}</p>`);
-}
-
-function shuffle(array) {
-  var counter = array.length;
-
-  while (counter > 0) {
-    var index = Math.floor(Math.random() * counter);
-
-    counter--;
-
-    var temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-
-  return array;
-}
-
-// David,Daniel,Jack,Harper,Madhu,Funke,Charles,Eric,Scott,Jean,Barb
+// Test Set: David,Daniel,Jack,Harper,Madhu,Funke,Charles,Eric,Scott,Jean,Barb
